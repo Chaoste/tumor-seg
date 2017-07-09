9522b8342dd351a77520a44a2aecf6f6d55a0761
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-07-09 22:06:22 +0200

    Add samples and Brain viz

--- WebApp/src/components/AnalyzingPage.jsx
+++ WebApp/src/components/AnalyzingPage.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import BrainView from './BrainView';
 
 export default class AnalyzingPage extends Component {
   render() {
     return (
-      <div>This is the analyzing Page</div>
+      <div>
+        <BrainView />
+        This is the analyzing Page
+      </div>
     );
   }
 }
