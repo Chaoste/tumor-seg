94e65b711edd4d2298b28d4f7adaf92444e50455
Thomas Kellermeier <t-kellermeier@hotmail.de>
2017-07-09 14:39:49 +0200

    Add ImageUploade code

--- WebApp/src/components/BrainView.jsx
+++ WebApp/src/components/BrainView.jsx
@@ -12,8 +12,14 @@ export default class BrainView extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (this.imageWasUploaded(nextProps)) {
+
       // TODO:
-      // 1.Load any image from output
+      // 1. Load any image from input directory
+      // 2. Add image to paper.js
+      // 3. Load output belonging to this input
+      // 4. Add output to paper.js as pixelwise components
+      // 5. Add onHover events to tumor regions
+      // 6.
     }
   }
 
@@ -24,6 +30,7 @@ export default class BrainView extends Component {
   render() {
     return (
       <div className='brain-container'>
+        <img id="upload" className="hidden" src="input/" width="400" height="400" ref={(img) => { this.img_container = img }} />
         <canvas id="brain-panel" ref={(canvas) => { this.canvas = canvas }} />
       </div>
     );
